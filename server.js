3bf46e23b1af39bfbb85426212f66fe639d17d62
Zollo <lxzw1988@gmail.com>
2019-11-12 21:54:38 +0300

    Initial Express setup

--- server.js
+++ server.js
@@ -1,51 +1,16 @@
-const http = require('http');
+const express = require("express");
+const dotenv = require("dotenv");
 
-const todos = [
-  { id: 1, text: 'TODO 1' },
-  { id: 2, text: 'TODO 2' },
-  { id: 3, text: 'TODO 3' }
-];
-
-const server = http.createServer((req, res) => {
-  const { method, url } = req;
-
-  let body = [];
-  req
-    .on('data', chunk => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
-
-      let status = 404;
-      const response = {
-        success: false,
-        data: null
-      };
-
-      if (method === 'GET' && url === '/todos') {
-        status = 200;
-        response.success = true;
-        response.data = todos;
-      } else if (method === 'POST' && url === '/todos') {
-        status = 200;
-        const { id, text } = JSON.parse(body);
-        todos.push({ id, text });
-        status = 201;
-        response.success = true;
-        response.data = todos;
-      }
-
-      res.writeHead(status, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js'
-      });
-      res.end(JSON.stringify(response));
-    });
+// - Load env vars
+dotenv.config({
+  path: "./config/config.env"
 });
 
-const PORT = 5000;
+// - app init
+const app = express();
 
-server.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
