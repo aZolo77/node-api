cd1b712b536bdaf13ff7aae8cf307a3bc2e1eb2a
Zolo <a.burtsev@hostland.ru>
2019-11-13 16:34:34 +0300

    Added server initialization with express, logger middleware, routes and controllers for bootcamps, mongoose setup for connection to MongoDB and bootcamp model

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 ## Links:
 
-- [Video Course](https://coursehunter.net/course/node-js-api-master-klass-s-express-i-mongodb) [14]
+- [Video Course](https://coursehunter.net/course/node-js-api-master-klass-s-express-i-mongodb) [21]
 - [API Specification](https://gist.github.com/bradtraversy/01adb248df70fb29e98c30cf659042cf)
 - [API Documentation](http://devcamper.io/)
 - [Finished API](https://github.com/bradtraversy/devcamper-api)
@@ -12,9 +12,15 @@
 - Node (NPM)
 - Git
 - [Postman](https://www.getpostman.com/)
+- [MongoDB](https://www.mongodb.com/)
+  > using Compass as GUI in Development mode and MongoDB.Atlas as cloud for DB
 
 ## Extensions:
 
 - [Synchronize Settings](https://artslab.info/vscode/sync-vs-code-settings-between-devices)
 - [Settings Gist](https://gist.github.com/aZolo77/50d2806dc21a142c0a6dad3288b48673)
 - [Generate Token](https://github.com/settings/tokens/)
+
+## Dependencies:
+
+- [mongoose](https://mongoosejs.com/)
--- server.js
+++ server.js
@@ -1,22 +1,49 @@
+// - dependencies
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
 
-// - Routes
-const bootcamps = require("./routes/bootcamps");
+// * middleware
+// - logger
+const morgan = require("morgan");
 
-// - Load env vars
+// - DB connection
+const connectDB = require("./config/db");
+
+// - load env vars
 dotenv.config({
   path: "./config/config.env"
 });
 
+// - connect to DB
+connectDB();
+
+// - routes
+const bootcamps = require("./routes/bootcamps");
+
 // - app init
 const app = express();
 
-// ? Mount routers
+// * dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// * mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+});
+
+// - handle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red.underline.italic);
+
+  // > close Server and exit proccess
+  server.close(() => process.exit(1));
 });
